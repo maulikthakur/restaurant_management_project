158d88cda9add51321b3516e501a720bad0f7a99
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-08-03 20:32:36 +0530

    17869

--- account/views.py
+++ account/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 # Create your views here.
+
+
+
+def some_view(request):
+    user = User.objects.get(username='example_user')
+    profile = user.profile
+    phone = profile.phone_number
+    
+
