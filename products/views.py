8b3d78cf1d59d75ba23df4b1dcf6748027769e44
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-08-04 20:46:43 +0530

    17889

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from .models import MenuItem
 from .models import Item
 from .serializers import ItemSerializer
 
