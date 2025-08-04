3fa7086ac1bdbeca5d46a6a2644a266de3f0b8c3
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-08-04 19:45:46 +0530

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import ProductListAPIView
 from .views import HardcodedMenuAPIView
+from django.urls import path, include
 
 urlpatterns = [
     path('api/menu/', ProductListAPIView.as_view(), name='menu-api'),
     path('api/menu/', HardcodedMenuAPIView.as_view(), name='hardcoded-menu-api'),
+    path('products/', include('products.urls')),
     
 ]
+
 
