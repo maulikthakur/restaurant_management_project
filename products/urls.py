f32d63dcb73db0eb0d6edc671a2d5d50761f8406
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-08-04 20:51:08 +0530

    17889

--- products/urls.py
+++ products/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('api/menu/', ProductListAPIView.as_view(), name='menu-api'),
     path('api/menu/', HardcodedMenuAPIView.as_view(), name='hardcoded-menu-api'),
     path('products/', include('products.urls')),
+    path('menu/', menu_view, name='maenu-page')
     
 ]
 
