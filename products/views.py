7088c7b1500746bfd9be81c069ba882317f40fba
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-08-04 19:43:58 +0530

    Update views.py

--- products/views.py
+++ products/views.py
@@ -9,3 +9,29 @@ class ProductListAPIView(APIView):
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class HardcodedMenuAPIView(APIView):
+    def get(self, request):
+        menu = [
+            {
+                "name": "Margherita Pizza",
+                "description": "Classic pizza with tomato sauce and mozzarella cheese",
+                "price": 249.00
+            },
+            {
+                "name": "Paneer Tikka",
+                "description": "Grilled paneer cubes with spicy marinade",
+                "price": 199.00
+            },
+            {
+                "name": "Masala Dosa",
+                "description": "Crispy dosa filled with spiced mashed potatoes",
+                "price": 129.00
+            }
+        ]
+        return Response(menu, status=status.HTTP_200_OK)
+
+
