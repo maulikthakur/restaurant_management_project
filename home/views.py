cd07e0de1e2346cf7e3dd55e0f8d2abe4f213a82
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-08-04 20:30:09 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # Redirect to clear the form
+        else:
+            form = ContactForm()
+
     return render(request,"homes.html")
+
