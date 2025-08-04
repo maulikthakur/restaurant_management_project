154ff74950a2dfd5213b7c98424778b96bf0c477
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-08-04 19:41:58 +0530

    Update admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,16 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
-from .models import UserProfile
+from .models import Product
 
-class UserProfileInline(admin.StackedInline):
-    model = UserProfile
-    can_delete = False
-    verbose_name_plural = 'Profile'
-
-class UserAdmin(BaseUserAdmin):
-    inlines = (UserProfileInline,)
-
-# Re-register UserAdmin
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price')
+    search_fields = ('name',)
