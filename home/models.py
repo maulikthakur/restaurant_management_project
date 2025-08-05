76c6f98880c6e8baf65f6651a2bb19eeb38010e8
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-08-05 12:18:19 +0530

    21602

--- home/models.py
+++ home/models.py
@@ -6,7 +6,7 @@ class Contact(models.Model):
         submitted_at = models.DateTimeField(auto_now_add=True)
 
         def __str__(self):
-            return f"{self.name} ({self.email})"
+            return f"{self.name} - {self.email}"
 
 class RestaurantInfo(models.Model):
         name = models.CharField(max_length=100)
