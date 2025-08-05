59ddc56e1e4d83fc5a0b7f957c087a6265bc137d
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-08-05 08:50:07 +0530

    17890

--- home/views.py
+++ home/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here:
+from django.conf import settings
+
 def home(request):
+    from .forms import ContactForm
+
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')  # Redirect to clear the form
+            return redirect('home')
         else:
             form = ContactForm()
 
-    return render(request,"homes.html")
+        context = {
+            'form': form,
+            'restaurant_name': settings.restaurant_name    
+        }
+    return render(request, 'homes.html', context)
+
+                                                                                }
 
