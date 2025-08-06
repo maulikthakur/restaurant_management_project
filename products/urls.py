9591d9fb2adb6133704b46ef7d5ca9ffa0c90319
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-08-06 11:56:55 +0530

    21601

--- products/urls.py
+++ products/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from .views import ProductListAPIView
 from .views import HardcodedMenuAPIView
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('api/menu/', ProductListAPIView.as_view(), name='menu-api'),
     path('api/menu/', HardcodedMenuAPIView.as_view(), name='hardcoded-menu-api'),
     path('products/', include('products.urls')),
-    path('menu/', menu_view, name='maenu-page')
+    path('menu/', views.menu_view, name='menu')
     
 ]
 
