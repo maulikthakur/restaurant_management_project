07f297b8ccd7f796e77bbac6665d0f1a2ece7571
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-08-06 11:52:27 +0530

    21601

--- products/models.py
+++ products/models.py
@@ -31,6 +31,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
 
     def __str__(self):
         return self.name
