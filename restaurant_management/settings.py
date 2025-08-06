1c7bbd198d94834d2aad81a24a8b31e87bb50d92
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-08-06 11:53:36 +0530

    21601

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,6 +29,10 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 RESTAURANT_NAME = 'Tasty Bites Restaurant'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # Application definition
