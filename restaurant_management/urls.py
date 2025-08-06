7c5c3628b5da38fe3c0a63c50d7e08c296ff28a9
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-08-06 11:54:40 +0530

    21601

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -16,15 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
-from django.urls import path, include
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('products.urls')),
     path('api/',include('home.urls')),
     path('api/accounts/',include('account.urls')),
     path('api/products/',include('products.urls')),
     path('api/orders/',include('orders.urls')),
 ]
-
