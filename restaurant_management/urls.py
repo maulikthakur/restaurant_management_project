58bd1a459ae1d084fc47612c1dbd7df2252e6db8
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-08-06 12:10:51 +0530

    21600

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +28,6 @@ urlpatterns = [
     path('api/products/',include('products.urls')),
     path('api/orders/',include('orders.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)ettings.MEDIA_ROOT)
