47faf61c8ef63c1b512c4683b6e3fad687904f44
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-08-10 12:55:00 +0530

    26504

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from .views import *
+from django.conf.urls import handler404
+from django.shortcuts import render
+
+def custom_404(request, exception):    
+    return render(request, '404.html', status=404)
+
+handler404 = custom_404
 
 urlpatterns = [
     path('',home, name ='home'),
