0071a1273b12d93460d42b4c6e703169bf9f2e10
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-08-14 13:13:48 +0530

    29126

--- home/views.py
+++ home/views.py
@@ -18,22 +18,12 @@ def home(request):
 
     context = {
         'form': form,
-        'restaurant_name': settings.restaurant_name,
+        'restaurant_name': settings.RESTAURANT_NAME,
+        'phone_number':settings.RESTAURANT_PHONE
         'restaurant_address': restaurant.address if restaurant and restaurant.address else 'Babariya Near Babariya Lake Road, Seoni, Madhya Pradesh (480661)'   
     }
 
     return render(request,'homes.html', context)
-
-# views.py
-from django.shortcuts import render
-
-def contact_us(request):
-    contact_info = {
-        "phone": "+91 9876543210",
-        "email": "info@restaurant.com",
-        "address": "Babariya Near Babariya Lake Road, Seoni, Madhya Pradesh (480661)"
-    }
-    return render(request, "contact_us.html", {"contact_info": contact_info})
 
 
 
