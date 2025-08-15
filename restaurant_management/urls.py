b83649a74af75d25d01f25f6820fa358b62d7ab1
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-08-15 14:03:14 +0530

    32724

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('api/accounts/',include('account.urls')),
     path('api/products/',include('products.urls')),
     path('api/orders/',include('orders.urls')),
+    path('reservations/', views.reservations, name='reservations'),
 ]
 
 if settings.DEBUG:
