282bb5e909a4bc6ede150699cbeb337d07348bbe
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-09-23 21:05:32 +0530

    47753

--- home/models.py
+++ home/models.py
@@ -19,4 +19,13 @@ class MenuCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)    
     
     def __str__(self):        
-        return self.name            
+        return self.name   
+
+class MenuItem(models.Model):    
+    name = models.CharField(max_length=150)    
+    description = models.TextField(blank=True, null=True)    
+    price = models.DecimalField(max_digits=8, decimal_places=2)    
+    category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE, related_name="items")    
+    
+    def __str__(self):        
+        return self.name        
