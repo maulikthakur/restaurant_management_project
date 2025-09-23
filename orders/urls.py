abaec744a431b6dbf8bbb04228e47b482812f114
Maulik Thakur <136841996+maulikthakur@users.noreply.github.com>
2025-09-23 21:02:54 +0530

    47754

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
 
+
 urlpatterns = [
-    path('categories/', MenuCategoryListView.as_view(), name='menu-categories')  
+    path('categories/', MenuCategoryListView.as_view(), name='menu-categories'),
+    path("order-history/", OrderHistoryView.as_view(), name="order-history"),  
 ]
